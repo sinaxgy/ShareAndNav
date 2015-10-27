a44e7764e8dc7c9d08fc7f845478a5cf5d8089f9
sinaxgy <191127340.qq.com>
2015-10-27 15:15:16 +0800

    before map

---
+++ ShareAndNav/Classes/Model/TypeDefine.swift
@@ -0,0 +1,27 @@
+//
+//  TypeDefine.swift
+//  ShareAndNav
+//
+//  Created by 徐成 on 15/10/26.
+//  Copyright © 2015年 徐成. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+//MARK:-- const value
+
+let XuTextSizeSmall:CGFloat = 12.0
+let XuTextSizeMiddle:CGFloat = 15.0
+let XutextSizeBig:CGFloat = 20
+
+let XuColorBlueThin = UIColor(red: 127/255, green: 193/255, blue: 1, alpha: 1)
+let XuColorBlue = UIColor(red: 0, green: 132/255, blue: 1, alpha: 1)
+let XuColorGray = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1)
+
+//MARK: -- enum and struct
+enum XuLoginType {
+    case DynamicCode,Password,Default
+}
+
