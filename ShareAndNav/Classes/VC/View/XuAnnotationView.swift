1927f4daf5f7f46adfd04cf9c7c7aad094d82f29
xubupt218 <191127340@qq.com>
2015-12-29 14:48:41 +0800

    branch

---
+++ ShareAndNav/Classes/VC/View/XuAnnotationView.swift
@@ -0,0 +1,21 @@
+//
+//  XuAnnotationView.swift
+//  ShareAndNav
+//
+//  Created by xubupt218 on 15/12/28.
+//  Copyright © 2015年 徐成. All rights reserved.
+//
+
+import UIKit
+
+class XuAnnotationView: MAAnnotationView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
